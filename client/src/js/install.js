8dbd2cc4ea2769885c4c59640d84d618f1530350
natesanow <152323081+natesanow@users.noreply.github.com>
2024-04-30 15:29:49 -0700

    bug fixes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,16 +9,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-   const promptEvent = window.differedPrompt;
+   const promptEvent = window.deferredPrompt;
    if (!promptEvent) {
     return;
    } 
    promptEvent.prompt();
-   window.defferedPrompt = null;
+   window.deferredPrompt = null;
    butInstall.classList.toggle('hidden', true)
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.defferedPrompt = null;
+    window.deferredPrompt = null;
 });
