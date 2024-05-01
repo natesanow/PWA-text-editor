9191bab00fd57ba796e2f694867b083414fc5b03
natesanow <152323081+natesanow@users.noreply.github.com>
2024-05-01 16:38:49 -0700

    bug fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,7 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
       new WebpackPwaManifest({ 
         fingerprints: false,
         inject: true,
@@ -47,18 +48,20 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS loaders
       rules: [
         {
-          test: /\.css$/,
+          test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset.env'],
+              presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
